0de37e0b3a7de76b595e836da16f04f29db563e2
vis1003 <vis21cs@cmrit.ac.in>
2023-07-30 23:25:38 +0530

    program 5

---
+++ 4th semester/Python Lab/program5b.py
@@ -0,0 +1,18 @@
+import re
+
+phno_pattern = re.compile(r'\+\d{12}$')
+#email_pat = re.compile(r'\S+\@\S+')
+email_pattern = re.compile(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
+email = []
+phno = []
+with open('file.txt','r') as f:
+    for line in f:
+        email.extend(email_pattern.findall(line))
+        phno.extend(phno_pattern.findall(line))
+
+print("Email IDs:")
+for x in email:
+    print(x) 
+print("Phone Numbers:")
+for x in phno:
+    print(x) 
