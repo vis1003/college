c9ada2bf4925dc65847e18bbf102dcf1d9ad39df
Vismitha S <116023873+vis1003@users.noreply.github.com>
2024-05-02 09:53:06 +0530

    Update views.py

--- 6th semester/Full Stack Development Lab/views.py
+++ 6th semester/Full Stack Development Lab/views.py
@@ -1,3 +1,4 @@
+#shownameapp
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,25 @@ from django.http import HttpResponse
 
 def myFunc(request):
     return HttpResponse("<h1>My name is Vismitha<h1>")
+
+#prog1 app
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def votefromhome(request,age):
+    if(age > 60):
+        diff = age-60
+        return HttpResponse("You can vote from home because you are <b>%s</b> years more than threshold age"%(diff))
+    
+    else:
+        diff = 60-age
+        return HttpResponse("You cannot vote from home because you are <b>%s</b> years less than threshold age"%(diff))
+    
+def checkif18(request,age):
+    if(age>=18):
+        return HttpResponse("You are eligible to vote as you are <b>%s</b> years more than threshold age"%(age-18))
+    
+    else:
+        return HttpResponse("You are not eligible to voteas you are <b>%s</b> years less than threshold age"%(18-age))
