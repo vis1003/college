16bae72d3599a6639952ce8c7d67596517b9e265
Vismitha S <116023873+vis1003@users.noreply.github.com>
2024-05-09 09:39:59 +0530

    Create views.py

---
+++ 6th semester/Full Stack Development Lab/Program 4/views.py
@@ -0,0 +1,12 @@
+import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def displaydateoffset(request):
+    a=datetime.datetime.now()
+    before=a-datetime.timedelta(hours=4)
+    after=a+datetime.timedelta(hours=4)
+    r1="<h2 style='color:red'><center>Date and time 4 hours before is %s <center></h2>"%(before)
+    r2="<h2 style='color:red'><center>Date and time 4 hours after is %s <center></h2>"%(after)
+    return HttpResponse(r1+r2)
